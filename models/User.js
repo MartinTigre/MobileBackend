533a0dc942a29964cf88172c9e1241f67a4f4853
MartinTigre <ahmetnejdetmeseci@gmail.com>
2022-11-24 16:58:22 +0300

    user model added

--- models/User.js
+++ models/User.js
@@ -1,1 +1,38 @@
 const mongoose = require("mongoose");
+
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+        minlength: 3,
+        maxlength:50,
+        index: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true,
+        minlength:5,
+        maxlength:255,
+        index:true
+    },
+    password:{
+        type: String,
+        rquired:true,
+        minlength:6,
+        maxlength:1024,
+        index:true
+    },
+    isManager:{
+        type:Boolean,
+        default:false,
+        index:true
+    }
+});
+
+
+
+const User = mongoose.Model("User", UserSchema);
+
+exports.User = User;
